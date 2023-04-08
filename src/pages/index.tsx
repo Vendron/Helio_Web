c6ce6e366453d5d13f2e353401b7ddaba774599d
Vendron <40866135+Vendron@users.noreply.github.com>
2023-04-08 01:12:59 +0100

    add User and Posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -18,8 +18,20 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div>{!user.isSignedIn && <SignInButton />}{!!user.isSignedIn && <SignOutButton />}</div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold text-white">Posts</h1>
+          <ul>
+            {data?.map((post) => (
+              <li key={post.id}>
+                  <a className="text-white">{post.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </main>
     </>
   );
